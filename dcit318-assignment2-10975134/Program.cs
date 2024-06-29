2d63d8930e2cd7f22b88f0dda330cef3960c9064
Clement Olives <olitech1010@gmail.com>
2024-06-29 14:50:49 +0000

    inheritance and method overriding
    Created Animal, Dot and Cat classes and implemeted in Program.cs (main)

--- dcit318-assignment2-10975134/Program.cs
+++ dcit318-assignment2-10975134/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        genericAnimal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
